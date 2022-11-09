2cc15cf1661bcdc1284a6f57b2492a8c8ba533e6
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-09 21:48:26 +0900

    22 11 09 - build project

---
+++ src/main/java/com/mustache/bbs3/SpringbootMustacheBbs3ExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustacheBbs3ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMustacheBbs3ExerciseApplication.class, args);
+	}
+
+}
